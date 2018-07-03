669ba75be6c287edaf328e256f86d1ed4b5995cf
Ekalaivan <kalai@logicsoft.co.in>
2018-07-03 14:06:44 +0530

    sample project for the issue https://github.com/lecaillon/Evolve/issues/67

---
+++ ServiceEvolveTest/ServiceEvolveTest/Migrations/V0_0_1__initial_migration.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE dbconfig ( 
+	record TEXT NOT NULL);
+
+CREATE TABLE preferences ( 
+	key TEXT NOT NULL, 
+	config TEXT NOT NULL, 
+	lastupdatedby TEXT,
+	lastupdatedon timestamp);
+
+
+CREATE TABLE app_exceptions ( 
+	id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	source TEXT,
+	StackTrace TEXT,
+	Message TEXT,
+	Data TEXT,
+	InnerExceptionMessage TEXT,
+	TimeStamp DATETIME,
+	Params TEXT );
